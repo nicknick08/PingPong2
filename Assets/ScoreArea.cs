cfed0606e5885d861afc22136f997b376ed2b038
nicknick08 <derekchiu12@gmail.com>
2022-01-31 16:06:24 +0900

    Initial commit

---
+++ Assets/ScoreArea.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreArea : MonoBehaviour
+{
+    public GameManager GameManager;
+    public int agentId;
+
+    void OnTriggerEnter(Collider other){
+        GameManager.EndEpisode(agentId);
+    }
+}
